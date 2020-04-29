38b2086aa49aba39e138f475b2b99acbdb2b81a1
Tony Sims <zocle@zocle.net>
2020-04-29 03:08:04 +0000

    changing back to c in placeholder

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
         x = ord(x)
         if x > 96 and x < 123:
             x = x - 32
-        print('{:s}'.format(x), end='')
+        print('{:c}'.format(x), end='')
     print()
