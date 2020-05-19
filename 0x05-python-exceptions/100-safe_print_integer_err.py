781bf37d45eba229d8e671f068ebe55f2a96a9f6
Tony Sims <zocle@zocle.net>
2020-05-19 00:31:54 +0000

    adding 100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,7 @@
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as err:
+        print('Exception:', err)
+        return False
+    return True
