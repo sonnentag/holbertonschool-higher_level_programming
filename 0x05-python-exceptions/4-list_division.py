4143cb26ef6ae643b2a60db60b44c2eb4678451a
Tony Sims <zocle@zocle.net>
2020-05-19 00:25:48 +0000

    changed some element names for readability

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    new_list = []
-    for c in range(list_length):
-        new = 0
+    result_list = []
+    for i in range(list_length):
+        result = 0
         try:
-            new = (my_list_1[c] / my_list_2[c])
+            result = (my_list_1[i] / my_list_2[i])
         except TypeError:
             print("wrong type")
         except ZeroDivisionError:
@@ -12,5 +12,5 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
         finally:
-            new_list.append(new)
-    return (new_list)
+            result_list.append(result)
+    return (result_list)
