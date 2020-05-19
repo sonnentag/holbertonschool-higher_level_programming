c9a206327799ac5c7188410ab6177d44689f7572
Tony Sims <zocle@zocle.net>
2020-05-19 01:11:25 +0000

    changed err from print to sys.stderr.write

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
     except Exception as err:
-        print("Exception: {}".format(err))
+        sys.stderr.write("Exception: {}\n".format(err))
         return (False)
     return (True)
