caf60446593b2d0dc3e4a67ca2c4aebf8d2f4c86
Tony Sims <zocle@zocle.net>
2020-05-19 01:13:59 +0000

    changed err from print to sys.stderr.write

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_function(fct, *args):
+    import sys
     try:
         ret = fct(*args)
     except Exception as err:
-        print('Exception:', err)
+        sys.stderr.write("Exception: {}\n".format(err))
         ret = None
     return(ret)
