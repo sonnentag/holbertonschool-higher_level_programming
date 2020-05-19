c78678bf858141812060289d650bac384e6ef1e8
Tony Sims <zocle@zocle.net>
2020-05-19 00:52:39 +0000

    added format to print err

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,6 +3,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except Exception as err:
-        print('Exception:', err)
+        print("Exception: {}".format(err))
         return (False)
     return (True)
