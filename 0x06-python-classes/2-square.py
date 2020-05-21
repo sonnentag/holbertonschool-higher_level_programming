a3204c894d3a4dd645ac763483c04cc1f6aef5f9
Tony Sims <zocle@zocle.net>
2020-05-21 19:28:03 +0000

    fixed typo of _size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -12,4 +12,4 @@ class Square:
         elif size < 0:
             raise ValueError('size must be >= 0')
         else:
-            self._Square__size = _size
+            self._Square__size = size
