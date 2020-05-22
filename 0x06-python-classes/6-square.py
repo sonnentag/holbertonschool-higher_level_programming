7aeaddcc28480cc29e9c7b1f3dd7607d19bf6bd5
Tony Sims <zocle@zocle.net>
2020-05-22 00:36:34 +0000

    fixed line indents per pep8

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,9 +18,9 @@ class Square:
                 'position must be a tuple of 2 positive integers')
 
         if type(position[0]) != int or type(position[1]) != int\
-            or position[0] < 0 or position[1] < 0:
-                raise TypeError(
-                    'position must be a tuple of 2 positive integers')
+                or position[0] < 0 or position[1] < 0:
+            raise TypeError(
+                'position must be a tuple of 2 positive integers')
         else:
             self.__position = position
 
@@ -39,9 +39,9 @@ class Square:
                 'position must be a tuple of 2 positive integers')
 
         if type(value[0]) != int or type(value[1]) != int\
-            or value[0] < 0 or value[1] < 0:
-                raise TypeError(
-                    'position must be a tuple of 2 positive integers')
+                or value[0] < 0 or value[1] < 0:
+            raise TypeError(
+                'position must be a tuple of 2 positive integers')
         else:
             self.__position = value
 
