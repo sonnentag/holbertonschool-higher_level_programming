c2d57fa88d5ce41caa8e2c99788d643834b3516e
Tony Sims <zocle@zocle.net>
2020-05-27 02:25:20 +0000

    fixed typo in valueerror str

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -51,6 +51,6 @@ class Rectangle:
         if type(value) != int:
             raise TypeError("height must be an integer")
         elif value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         else:
             self.__height__ = value
