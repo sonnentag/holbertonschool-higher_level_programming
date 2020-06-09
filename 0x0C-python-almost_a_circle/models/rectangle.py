1139c59846ca28f6aae5aaa96437a6cf875ff31b
Tony Sims <zocle@zocle.net>
2020-06-09 18:05:46 +0000

    fixed >= onb height/width setters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -50,8 +50,8 @@ class Rectangle(Base):
 
         if type(value) != int:
             raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
         else:
             self.__width = value
 
@@ -67,8 +67,8 @@ class Rectangle(Base):
 
         if type(value) != int:
             raise TypeError("height must be an integer")
-        elif value < 0:
-            raise ValueError("height must be >= 0")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
         else:
             self.__height = value
 
