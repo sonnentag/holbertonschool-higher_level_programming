e046d59583bb29467718929cb8a42ddb85d2711e
Tony Sims <zocle@zocle.net>
2020-07-14 20:24:09 -0500

    second comment for some reason

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- show grants for these 2 users
+-- show grants for these 2 users, user_0d_[12]
+-- not sure why I need a second comment here 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
