4675ae03b9e1754bbe62bcaecff15164ec0d4650
Tony Sims <zocle@zocle.net>
2020-07-14 23:14:02 -0500

    this doesn't run locally, error 1215

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,3 +1,3 @@
 -- creates hbtn_0d_usa db and cities table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities( id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`(id INT AUTO_INCREMENT NOT NULL PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, FOREIGN KEY (state_id) REFERENCES `hbtn_0d_usa`.`states`(id));
