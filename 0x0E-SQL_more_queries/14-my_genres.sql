3b23030424edae00c01fafe8c65e2ae61847405a
Tony Sims <zocle@zocle.net>
2020-07-14 22:59:12 -0500

    un-mixed up table names

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
 -- list genres for TV show Dexter
-SELECT t.name FROM tv_genres t JOIN tv_show_genres g ON t.id = g.genre_id JOIN tv_shows s ON g.show_id = s.id WHERE s.title = "Dexter" ORDER BY g.name ASC;
+SELECT t.name FROM tv_genres t JOIN tv_show_genres g ON t.id = g.genre_id JOIN tv_shows s ON g.show_id = s.id WHERE s.title = "Dexter" ORDER BY t.name ASC;
