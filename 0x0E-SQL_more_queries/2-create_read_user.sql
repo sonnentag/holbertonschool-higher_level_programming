9c825d138c160bc9f686f935e93c1fd2f126cc11
Tony Sims <zocle@zocle.net>
2020-07-14 20:26:42 -0500

    added flush

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,3 +3,4 @@ CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 DROP USER IF EXISTS 'user_0d_2'@'localhost';
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
