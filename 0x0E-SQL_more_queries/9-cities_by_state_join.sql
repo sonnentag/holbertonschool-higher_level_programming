8c94e75837d8dee10110aff5a4daf836b475a6c2
Tony Sims <zocle@zocle.net>
2020-07-14 22:42:30 -0500

    trying this before adding JOIN

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- all cities
-SELECT c.id, c.name, s.name FROM cities c, states s where s.id = c.state_id ORDER BY c.id ASC;
+SELECT c.id, c.name, s.name FROM cities c, states s where s.id = c.state_id ORDER BY id ASC;
