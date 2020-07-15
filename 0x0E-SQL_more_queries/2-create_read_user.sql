02e0c1bec974c39c0399d08e5b80acb0cc25167a
Tony Sims <zocle@zocle.net>
2020-07-14 20:29:51 -0500

    maybe the checker is looking for a second comment line on this one?

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- create database, user, and grant select on new db to new user
+-- create database, user, and grant select on new db to new user
 CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
 DROP USER IF EXISTS 'user_0d_2'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
