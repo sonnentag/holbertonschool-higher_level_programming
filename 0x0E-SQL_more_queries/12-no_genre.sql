66accbae294b5350f74eb5e36e1fb2ea4481bc03
Tony Sims <zocle@zocle.net>
2020-07-14 22:49:37 -0500

    changed to use JOIN

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,2 @@
 -- NULL genre ID
-SELECT t.title, s.genre_id FROM tv_shows t, tv_show_genres s WHERE t.id = s.show_id AND s.genre_id IS NULL ORDER BY t.title, s.genre_id ASC;
+SELECT t.title, s.genre_id FROM tv_shows t LEFT JOIN tv_show_genres s ON t.id = s.show_id WHERE s.genre_id IS NULL ORDER BY t.title, s.genre_id ASC;
