188aaf0729e16ccef3f226d50f88b327553dd94f
Tony Sims <zocle@zocle.net>
2020-07-14 20:37:20 -0500

    removed IF NOT EXISTS for user create since I just dropped the user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- create database, user, and grant select on new db to new user
+-- create database, user, and grant select
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 DROP USER IF EXISTS 'user_0d_2'@'localhost';
 CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
