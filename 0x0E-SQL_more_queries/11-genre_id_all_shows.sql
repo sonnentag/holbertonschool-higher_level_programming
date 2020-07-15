1327c86557a1ca9ad5a5811df4902db0a1a837af
Tony Sims <zocle@zocle.net>
2020-07-14 22:47:59 -0500

    added LEFT

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,2 +1,2 @@
 -- all the genres, all the shows
-SELECT t.title, s.genre_id FROM tv_shows t JOIN tv_show_genres s ON t.id = s.show_id ORDER BY t.title, s.genre_id ASC;
+SELECT t.title, s.genre_id FROM tv_shows t LEFT JOIN tv_show_genres s ON t.id = s.show_id ORDER BY t.title, s.genre_id ASC;
