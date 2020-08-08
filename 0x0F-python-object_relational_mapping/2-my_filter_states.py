5ee9a87b7065e66792a788d5d0749e360c386f90
Tony Sims <zocle@zocle.net>
2020-08-07 23:36:57 -0500

    2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+""" lists all states from a database providing connection info as args"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+
+    db_user = argv[1]
+    db_pass = argv[2]
+    db_name = argv[3]
+    state = argv[4]
+
+    db = MySQLdb.connect("localhost", db_user, db_pass, db_name)
+
+    cur = db.cursor()
+
+    sql = "SELECT id, name FROM states WHERE name='" + state + \
+        "' ORDER BY id ASC"
+
+    cur.execute(sql)
+
+    for row in cur.fetchall():
+        print(row)
