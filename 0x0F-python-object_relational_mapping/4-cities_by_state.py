f47533f461b2dea78dd6f1f1511c06b35885e7e8
Tony Sims <zocle@zocle.net>
2020-08-08 00:59:21 -0500

    4

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" lists all states from a database providing connection info as args"""
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+
+    db_user = argv[1]
+    db_pass = argv[2]
+    db_name = argv[3]
+
+    db = MySQLdb.connect("localhost", db_user, db_pass, db_name)
+
+    cur = db.cursor()
+
+    sql = 'SELECT c.id, c.name, s.name FROM cities c, states s \
+        WHERE c.state_id = s.id ORDER BY id ASC'
+
+    cur.execute(sql)
+
+    for row in cur.fetchall():
+        print(row)
