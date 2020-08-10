b7e555832d9593bc367e5f72adcbc9cbb2d8dbbe
Tony Sims <zocle@zocle.net>
2020-08-09 23:48:58 -0500

    reverted base decl

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,6 +5,8 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 
 from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
+
 
 class City(Base):
     ''' City '''
