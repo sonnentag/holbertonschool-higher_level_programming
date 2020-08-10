29d529bae70667b78c33aee4b4db38fabc6032ab
Tony Sims <zocle@zocle.net>
2020-08-09 20:46:40 -0500

    13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" update name of state with id 2 to New Mexico """
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == '__main__':
+    """ main """
+
+    db_user = argv[1]
+    db_pass = argv[2]
+    db_name = argv[3]
+
+    db = 'mysql+mysqldb://{}:{}@localhost/{}'.format(db_user, db_pass, db_name)
+
+    Session = sessionmaker(bind=create_engine(db))
+
+    sess = Session()
+
+    res = sess.query(State).filter(State.name.like('%a%'))
+
+    for row in res:
+        sess.delete(row)
+
+    sess.commit()
+
+    sess.close()
