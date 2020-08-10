5e287920ab1c831c09676a88ed82d3acdd52229d
Tony Sims <zocle@zocle.net>
2020-08-09 23:46:12 -0500

    removed base imported from state

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,7 +5,6 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base()
 
 class City(Base):
     ''' City '''
