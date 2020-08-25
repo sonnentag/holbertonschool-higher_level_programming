5832dcaffa1273d466d9a0579d5ed6ec364baf5a
Tony Sims <zocle@zocle.net>
2020-08-24 23:12:28 -0500

    main, pep8

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,10 +4,14 @@
 from sys import argv
 from urllib import request, parse
 
-url = argv[1]
-email = argv[2]
 
-data = parse.urlencode({'email': email})
+if __name__ == '__main__':
+    ''' main '''
 
-with request.urlopen(url, data.encode('utf-8')) as response:
-    print(response.read().decode('utf8'))
+    url = argv[1]
+    email = argv[2]
+
+    data = parse.urlencode({'email': email})
+
+    with request.urlopen(url, data.encode('utf-8')) as response:
+        print(response.read().decode('utf8'))
