935716bd1535099c7c3193e50a90fa06ba8d92e8
Tony Sims <zocle@zocle.net>
2020-08-24 22:43:36 -0500

    fixed print

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -9,14 +9,8 @@ data = parse.urlencode({'email': argv[2]})
 with request.urlopen(argv[1], data.encode('ascii')) as response:
     print(response.read().decode('utf8'))
 
-
     try:
         url = sys.argv[1]
         with urllib.request.urlopen(url) as response:
             html = response.read().decode('utf8')
             print(html)
-    except urllib.error.HTTPError as e:
-        # e = HTTP Error 401: UNAUTHORIZED
-        e = str(e)
-        e = e.rsplit(' ')[2].replace(':', '')
-        print('Error code: {}'.format(e))
