0c59f56d7b1717775a2785d2b8d9ab2022148ad9
Tony Sims <zocle@zocle.net>
2020-08-24 23:26:33 -0500

    getheader

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 """ get specific header from provided url """
+
 from urllib import request
 from sys import argv
+
 
 if __name__ == '__main__':
     ''' main '''
 
     with request.urlopen(argv[1]) as response:
-        headers = response.info()
-        print(headers('X-Request-Id'))
+        print(response.getheader('X-Request-Id'))
