d7219ab4c76590469baf168f977bf267db2def3c
Tony Sims <zocle@zocle.net>
2020-09-15 03:13:25 -0500

    simplified assignments

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // output according to number of args
 
-const fstArg = (process.argv[2]) ? process.argv[2] : undefined;
-const sndArg = (process.argv[3]) ? process.argv[3] : undefined;
+const fstArg = process.argv[2]
+const sndArg = process.argv[3]
 
 console.log(fstArg + ' is ' + sndArg);
