8a65f10df60999386d81c517a7e2aac42c42d070
Tony Sims <zocle@zocle.net>
2020-09-15 04:18:32 -0500

    adjusted whiles to remove redundant var

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,19 +2,19 @@
 //  print a square of arg size
 
 const size = process.argv[2];
-let count1 = 0;
-let count2 = 1;
+let count = 1;
 let output = 'x';
 
 if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  while (count1 < size) {
-    while (count2 < size) {
-      output = output + 'x';
-      count2 += 1;
-    }
+  while (count < size) {
+    output = output + 'x';
+    count += 1;
+  }
+  count = 0;
+  while (count < size) {
     console.log(output);
-    count1 += 1;
+    count += 1;
   }
 }
