d29f766af654d217cc5f29eb7c8680a44b539d8a
Tony Sims <zocle@zocle.net>
2020-09-15 20:48:51 -0500

    refactoring, removed unnecessary const's

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-// output according to number of args 
+// output according to number of args
 
-const argLen = process.argv.length;
-
-const myVar = (argLen === 2) ? 'No argument'
-  : (argLen === 3) ? 'Argument found'
-    : 'Arguments found';
-
-console.log(myVar);
+console.log((process.argv.length === 2) ? 'No argument'
+  : (process.argv.length === 3) ? 'Argument found'
+    : 'Arguments found');
