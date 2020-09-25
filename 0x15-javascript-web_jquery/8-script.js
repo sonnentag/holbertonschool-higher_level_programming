1cb60e250de5ea3dbfc10d1ae62976ba6e019e97
Elcoz <oi@localhost.localdomain>
2020-09-24 20:43:34 -0500

    removed unused 'index' and fixed semistandard 'template string expression' (vs other validator's warning of 'template literal syntax')

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,5 @@
 $.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
-  $(data.results).each(function (index) {
-    $('UL#list_movies').append(`<li>${this.title}</li>`);
+  $(data.results).each(function () {
+    $('UL#list_movies').append('<li>' + `${this.title}` + '</li>');
   });
 });
